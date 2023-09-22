0c2e52a652b915b7aece91f8d8d08e11b92755bb
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-09-22 14:02:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number: "))
 
 if (number % 2) == 0:
-    print(f"{0} is an even number")
+    print(f"{number} is an even number")
 else:
-    print(f"{0} is an odd number")
+    print(f"{number} is an odd number")

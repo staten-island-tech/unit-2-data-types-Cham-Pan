2df4097a01a03311f060b6d84658e13541ca22e3
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-09-27 13:16:48 -0400

    Update bill.py

--- bill.py
+++ bill.py
@@ -2,13 +2,13 @@ bill = 25
 x = input("Was the service great, good, okay, or bad?")
 
 if x == "great":
-    bill = bill*1.25
+    bill *= 1.25
     print(f"Your bill comes up to ${round(bill, 2)} with tip.")
 elif x == "good":
-    bill = bill*1.2
+    bill *= 1.2
     print(f"Your bill comes up to ${round(bill, 2)} with tip.")
 elif x == "okay":
-    bill = bill*1.15
+    bill *= 1.15
     print(f"Your bill comes up to ${round(bill, 2)} with tip.")
 else:
     print(f"Your bill comes up to ${round(bill, 2)}.")

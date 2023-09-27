3edc41940dca594ed330c680e184733d1147f8cc
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-09-27 13:35:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-bill = 25
-x = input("Was the service great, good, okay, or bad?")
+number = int(input("Enter a number: "))
 
-if x == "great":
-    bill = bill*1.25
-    print(f"Your bill comes up to ${round(bill, 2)} with tip.")
-elif x == "good":
-    bill = bill*1.2
-    print(f"Your bill comes up to ${round(bill, 2)} with tip.")
-elif x == "okay":
-    bill = bill*1.15
-    print(f"Your bill comes up to ${round(bill, 2)} with tip.")
-else:
-    print(f"Your bill comes up to ${round(bill, 2)}.")
+for i in range(1, number + 1):
+    if number % i == 0:
+        print(i)

dde33c8cfe4ff58daf30cda42c1709d34da2ad9e
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-09-28 13:54:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-numberx = int(input("Enter a number: "))
-numbery = int(input("Enter a number: "))
-
-for i in range(1, numberx + 1):
-    if numberx % i == 0:
-        print(i)
-
-for i in range(1, numbery + 1):
-    if numbery % i == 0:
-        print(i)
+square(20, 30, 30)

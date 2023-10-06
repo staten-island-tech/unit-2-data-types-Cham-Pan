e3b381a1ec52482b42291a2d6551af2651bbb36b
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-10-06 14:02:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-def hello():
-    print("wsg")
-hello()
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+
+if number1 < number2:
+    smaller_number = number1
+else:
+    smaller_number = number2
+
+for i in range(1, smaller_number + 1):
+    if (number1 % i == 0) and (number2 % i == 0):
+        gcf = i
+    else:
+        gcf = 1
+print(i)

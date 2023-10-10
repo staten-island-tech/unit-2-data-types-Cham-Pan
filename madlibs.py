5da76c9d13c0052b4b889152e7ebb8a27508aaba
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-10-10 13:36:28 -0400

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+food = input("Food: ")
+name = input("Name: ")
+adjective = input("Adjective: ")
+noun = input("Noun: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+
+madlibs = f"It was {food} day at school, and {name} was super {adjective} for lunch. But when she went outside to eat, a {noun} stoler her {food}! {name} chased the {noun} all over school. She {verb1}, {verb2}, and {verb3} through the playground. Then she tripped on her {noun} and the {noun} escaped! Luckily, {name}'s friends were willing to share their {food} with her."
+print(madlibs)

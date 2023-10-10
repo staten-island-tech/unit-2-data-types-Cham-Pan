9bbe7c2e5f4835b83b62876b14f784be55202307
Cham-Pan <144945585+Cham-Pan@users.noreply.github.com>
2023-10-10 14:00:52 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,5 +1,12 @@
-def gcf(x, y):
-    if x == 0:
-        return y
-    return gcf(y % x, x)
-print(gcf(1, 2))
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+
+if number1 < number2:
+    smaller_number = number1
+else:
+    smaller_number = number2
+
+for i in range(1, smaller_number + 1):
+    if (number1 % i == 0) and (number2 % i == 0):
+        gcf = i
+print(gcf)
